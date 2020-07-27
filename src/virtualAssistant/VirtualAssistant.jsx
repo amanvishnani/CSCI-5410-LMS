c692f2042198a3ec60e484e9c34197ba89b73b7a
Gaurav Anand <gr874432@dal.ca>
2020-07-27 00:15:03 -0300

    Add welcome text to Help page

--- src/virtualAssistant/VirtualAssistant.jsx
+++ src/virtualAssistant/VirtualAssistant.jsx
@@ -4,6 +4,11 @@ import LexChat from "react-lex";
 class VirtualAssistant extends Component {
   render() {
     return (
+        <>
+        <div style={{width:"100%", textAlign:"center"}}>
+            <h1>Welcome to Help Page</h1>
+            <h3>Use our Virtual Assistant to ask queries</h3>
+        </div>
       <LexChat
         botName="VirtualAssistance"
         IdentityPoolId="us-east-1:55f82bbf-ce41-4372-8029-5e41da3f6f13"
@@ -13,6 +18,7 @@ class VirtualAssistant extends Component {
         height="430px"
         headerText="Chat with our Virtual Assistant"
       />
+      </>
     );
   }
 }
