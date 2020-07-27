aeda61cbc5affc4ee4970a5a6ad872e21ac338e1
Aman Vishnani <aman.vishnani@dal.ca>
2020-07-27 12:23:58 -0300

    Added missing Logout ajax call.

--- src/logout/Logout.js
+++ src/logout/Logout.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+import Axios from 'axios'
+import { LOGOUT_URL } from '../BaseUrls'
 
 export default function Logout() {
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    let token = userData.token
     localStorage.removeItem("loggedIn")
     localStorage.removeItem("userData")
     localStorage.removeItem("email")
+    Axios.delete(`${LOGOUT_URL}`, {
+        headers: {
+            "Authorization": token
+        }
+    })
     return (
         <Redirect to="/login"></Redirect>
     )
