3977933cc026179f8e68e9de2d912f11935952bc
manoharan <pr971100@dal.ca>
2020-07-27 10:30:41 -0300

    button styles

--- src/sentiment-analyzer/SentimentAnalyzer.jsx
+++ src/sentiment-analyzer/SentimentAnalyzer.jsx
@@ -1,68 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import { GET_SENTIMENT_URL, INGEST_FILE_URL } from '../BaseUrls'
-import Axios from 'axios'
-import Loader from 'react-loader-spinner'
+import React, { useState, useEffect } from "react";
+import { GET_SENTIMENT_URL, INGEST_FILE_URL } from "../BaseUrls";
+import Axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function SentimentAnalyzer() {
-    const [loading, setLoading] = useState(false)
-    const [messages, setMessages] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([]);
 
-    async function ingestFile() {
-        setLoading(true)
-        await Axios.get(`${INGEST_FILE_URL}`)
-        setLoading(false)
+  async function ingestFile() {
+    setLoading(true);
+    await Axios.get(`${INGEST_FILE_URL}`);
+    setLoading(false);
+  }
+
+  async function getMessages() {
+    setLoading(true);
+    let r = await Axios.get(`${GET_SENTIMENT_URL}`);
+    let data = r.data;
+    setMessages(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getMessages();
+  }, []); // Component Did Mount
+
+  function renderButtons() {
+    if (loading) {
+      return (
+        <Loader
+          type="ThreeDots"
+          color="#00BFFF"
+          height={40}
+          width={40}
+          timeout={10000}
+        />
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <button
+            className="btn btn-info "
+            style={{
+              marginLeft: "1em",
+              marginTop: "1em",
+            }}
+            onClick={(_) => ingestFile()}
+          >
+            Ingest New Data
+          </button>
+          <button
+            className="btn btn-info "
+            style={{
+              marginLeft: "1em",
+              marginTop: "1em",
+            }}
+            onClick={(_) => getMessages()}
+          >
+            Refresh
+          </button>
+        </React.Fragment>
+      );
     }
+  }
 
-
-    async function getMessages() {
-        setLoading(true)
-        let r = await Axios.get(`${GET_SENTIMENT_URL}`);
-        let data = r.data;
-        setMessages(data)
-        setLoading(false)
-    }
-
-    useEffect(() => {
-        getMessages()
-    }, []) // Component Did Mount
-
-
-    function renderButtons() {
-        if (loading) {
-            return <Loader
-                type="ThreeDots"
-                color="#00BFFF"
-                height={40}
-                width={40}
-                timeout={10000}
-            />
-        } else {
-            return <React.Fragment>
-                <button onClick={_ => ingestFile()}>Ingest New Data</button>
-                <button onClick={_ => getMessages()}>Refresh</button>
-            </React.Fragment>
-        }
-    }
-
-    return (
-        <div>
-            {renderButtons()}
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Text</th>
-                        <th>Sentiment</th>
-                        <th>Confidence Score</th>
-                    </tr>
-                    {
-                        messages && messages.map((message, i) => <tr key={i}>
-                            <td>{message.text}</td>
-                            <td>{message.sentiment}</td>
-                            <td>{message.score}</td>
-                        </tr>)
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      {renderButtons()}
+      <table style={{ marginLeft: "2em", marginTop: "2em" }}>
+        <tbody>
+          <tr>
+            <th>Text</th>
+            <th>Sentiment</th>
+            <th>Confidence Score</th>
+          </tr>
+          {messages &&
+            messages.map((message, i) => (
+              <tr key={i}>
+                <td>{message.text}</td>
+                <td>{message.sentiment}</td>
+                <td>{message.score}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/upload-file/UploadFile.jsx
+++ src/upload-file/UploadFile.jsx
@@ -1,92 +1,101 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { findAll } from "./UploadFileService";
 import { GCF_BASE_URL } from "../BaseUrls";
 
 function UploadFile(props) {
-    const [files, setFiles] = useState([])
-    const [onlineFiles, setOnlineFiles] = useState(null)
+  const [files, setFiles] = useState([]);
+  const [onlineFiles, setOnlineFiles] = useState(null);
 
-    async function refreshFiles() {
-        try {
-            let res = await findAll();
-            let _files = [];
-            for (const f of res.files) {
-                _files.push({
-                    name: f.name,
-                    metadata: JSON.stringify(f.metadata)
-                })
-            }
-            setOnlineFiles(_files);
-        } catch (error) {
-            debugger
-            alert("Somthing went wrong while fetching file list")
-        }
+  async function refreshFiles() {
+    try {
+      let res = await findAll();
+      let _files = [];
+      for (const f of res.files) {
+        _files.push({
+          name: f.name,
+          metadata: JSON.stringify(f.metadata),
+        });
+      }
+      setOnlineFiles(_files);
+    } catch (error) {
+      debugger;
+      alert("Somthing went wrong while fetching file list");
     }
+  }
 
-    useEffect(() => {
-        // @TODO: Get Files
-        if (onlineFiles == null) {
-            refreshFiles();
-        }
-        return () => {
-
-        }
-    }, [onlineFiles])
-    async function uploadFiles() {
-        if (files.length === 0) {
-            alert("Please select file to upload")
-        } else {
-            const formData = new FormData()
-            let file = files[0]
-            let name = file.name;
-            formData.append(name, files[0])
-            // Ideally Should be in a Service call.
-            let resp = await fetch(`${GCF_BASE_URL}/uploadFile`, {
-                method: 'POST',
-                body: formData
-            })
-            let result = await resp.json();
-            debugger
-            if(result.message === "OK") {
-                alert("Upload Complete!")
-                refreshFiles()
-            }
-        }
+  useEffect(() => {
+    // @TODO: Get Files
+    if (onlineFiles == null) {
+      refreshFiles();
     }
+    return () => {};
+  }, [onlineFiles]);
+  async function uploadFiles() {
+    if (files.length === 0) {
+      alert("Please select file to upload");
+    } else {
+      const formData = new FormData();
+      let file = files[0];
+      let name = file.name;
+      formData.append(name, files[0]);
+      // Ideally Should be in a Service call.
+      let resp = await fetch(`${GCF_BASE_URL}/uploadFile`, {
+        method: "POST",
+        body: formData,
+      });
+      let result = await resp.json();
+      debugger;
+      if (result.message === "OK") {
+        alert("Upload Complete!");
+        refreshFiles();
+      }
+    }
+  }
 
-    return (
-        <div>
-            <h1>Upload file</h1>
-            <input onChange={(evt) => setFiles(evt.target.files)} type="file" id="fileUpload" />
-            <button onClick={_ => uploadFiles()}>Upload</button>
-            <br/>
-            <br/>
-            <button onClick={_=>refreshFiles()}>Refresh Files</button>
-            <br/>
-            <br/>
-            <table>
-                <thead>
-                    <tr>
-                        <th>File Name</th>
-                        <th>Metadata</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        onlineFiles &&
-                        onlineFiles.map(file => {
-                            return (<tr key={file.name}>
-                                <td>{file.name}</td>
-                                <td>{file.metadata}</td>
-                            </tr>)
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      <h1 style={{ marginLeft: "1em" }}>Upload file</h1>
+      <input
+        style={{ marginLeft: "1em", marginTop: "1em" }}
+        onChange={(evt) => setFiles(evt.target.files)}
+        type="file"
+        id="fileUpload"
+      />
+      <button className="btn btn-info" onClick={(_) => uploadFiles()}>
+        Upload
+      </button>
+      <br />
+      <br />
+      <button
+        style={{ marginLeft: "1em" }}
+        className="btn btn-info"
+        onClick={(_) => refreshFiles()}
+      >
+        Refresh Files
+      </button>
+      <br />
+      <br />
+      <table style={{ marginLeft: "1em" }}>
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>Metadata</th>
+          </tr>
+        </thead>
+        <tbody>
+          {onlineFiles &&
+            onlineFiles.map((file) => {
+              return (
+                <tr key={file.name}>
+                  <td>{file.name}</td>
+                  <td>{file.metadata}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-
-export default UploadFile
-
+export default UploadFile;
