9b4b865fb7d2374b6d07db7d8c5ac36f7555e943
manoharan <pr971100@dal.ca>
2020-07-27 10:21:10 -0300

    few formatting changes

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -19,18 +19,25 @@ class AppHeader extends Component {
   }
 
   renderLoggedInItems() {
-    let [name, orgName] = ['', '']
+    let [name, orgName] = ["", ""];
     try {
-      let ud = localStorage.getItem("userData")
+      let ud = localStorage.getItem("userData");
       let userData = JSON.parse(ud);
-      name = `${userData.user.firstName} ${userData.user.lastName}`
-      orgName = `${userData.org.name}`
-    } catch (error) {
-
-    }
+      name = `${userData.user.firstName} ${userData.user.lastName}`;
+      orgName = `${userData.org.name}`;
+    } catch (error) {}
     return (
       <React.Fragment>
-        <span style={{color:"white", marginRight: "100px"}}>{name}<br/>{orgName}</span>
+        {/* <span style={{ color: "white", marginRight: "100px" }}>
+          {name}
+          <br />
+          {orgName}
+        </span> */}
+        <Nav.Link className="text-light " href="/home">
+          {name}
+          <br />
+          {orgName}
+        </Nav.Link>
         <Nav.Link className="text-light font-weight-bold" href="/upload-file">
           Upload Files
         </Nav.Link>
--- src/home/Home.js
+++ src/home/Home.js
@@ -3,10 +3,42 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     console.log(localStorage.email);
-    return <div>
-      <h1>This is Homepage</h1>
-      <h2>Explore Different sections in the menu bar</h2>
-    </div>;
+    return (
+      <div style={{ background: "#e5ffff" }}>
+        <h1
+          style={{
+            marginTop: "1.5em",
+            marginLeft: "1.5em",
+            marginRight: "0.5em",
+            fontStyle: "italic",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Welcome to our web application.
+        </h1>
+        <h3 style={{ marginLeft: "4em", fontFamily: "sans-serif" }}>
+          We have below options to explore in our application.
+        </h3>
+        <ul style={{ textIndent: "6.5em", fontFamily: "sans-serif" }}>
+          <li style={{ paddingBottom: "0.5em" }}>
+            Upload Files section: To form a word cloud with named entities.
+          </li>
+          <li style={{ paddingBottom: "0.5em" }}>
+            Help section: To assist users in application usuage.
+          </li>
+          <li style={{ paddingBottom: "0.5em" }}>
+            Sentiment Analysis section: To detect the sentiment of the online
+            chat messages.
+          </li>
+          <li style={{ paddingBottom: "0.5em" }}>
+            Chat section: To chat with the online users.
+          </li>
+        </ul>
+        <h5 style={{ marginLeft: "6em", fontFamily: "sans-serif" }}>
+          Explore the various options using the navigational bar at the top.
+        </h5>
+      </div>
+    );
   }
 }
 
